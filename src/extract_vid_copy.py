41d8155fa40457cf1b2eaa79515c6273e8914c1d
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-08-28 19:56:29 -0500

    copy script

---
+++ src/extract_vid_copy.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+from colorama import init, Fore, Style
+
+init()
+print(Fore.BLUE + "Change to blue text.")
+
+def move_videos(source_dir, dest_dir):
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith((".mp4", ".mov", ".mkv", ".ts")):
+                source_path = os.path.join(root, file)
+                dest_path = os.path.join(dest_dir, file)
+                # The shutil.copy2 preserves both the file contents and metadata (such as timestamps) during the copy.
+                shutil.copy2(source_path, dest_path)
+                # shutil.move(source_path, dest_path)
+                print(f"Copied {file} to {dest_dir}")
+
+# Example usage
+source_directory = "/Users/ravi/Downloads/test"
+destination_directory = "/Users/ravi/Downloads/out_test_copy"
+move_videos(source_directory, destination_directory)
